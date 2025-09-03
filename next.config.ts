fc3dfb9f4dcca3e14a4e031ffc559e0e7445a1da
Arunim Chakraborty <arunimchakraborty@gmail.com>
2025-09-03 20:01:49 +0530

    added minio

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'minio-api.internal.wabisabitech.in',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    minimumCacheTTL: 60,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
 };
 
 export default nextConfig;
