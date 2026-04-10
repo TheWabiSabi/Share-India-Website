a6fcb1dcf74724094324efbd643f813d65624d9b
Arunim Chakraborty <arunimchakraborty@gmail.com>
2026-04-11 02:10:33 +0530

    Added services by shareindia page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -19,6 +19,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/services-by-shareindia`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly' as const,
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/contact`,
       lastModified: currentDate,
       changeFrequency: 'monthly' as const,
